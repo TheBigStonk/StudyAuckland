24410d87ea97285ac62f7ee1c4c2bdb38a04cd5c
TheBigStonk <64230005+TheBigStonk@users.noreply.github.com>
2025-07-08 00:27:43 +1200

    More locations and default logic changes

--- src/App.tsx
+++ src/App.tsx
@@ -113,10 +113,10 @@ function App() {
               </div>
               <Label className="hover:bg-accent/50 flex items-start gap-3 rounded-lg border p-3 has-[[aria-checked=true]]:border-blue-600 has-[[aria-checked=true]]:bg-blue-50 dark:has-[[aria-checked=true]]:border-blue-900 dark:has-[[aria-checked=true]]:bg-blue-950">
                 <Checkbox
-                  id="toggle-2"
-                  defaultChecked
+                  id="terms-6"
                   className="data-[state=checked]:border-blue-600 data-[state=checked]:bg-blue-600 data-[state=checked]:text-white dark:data-[state=checked]:border-blue-700 dark:data-[state=checked]:bg-blue-700"
-                  onCheckedChange={() => handleTagToggle("Free wifi")}
+                  checked={selectedTags.includes("Free WiFi")}
+                  onCheckedChange={() => handleTagToggle("Free WiFi")}
                 />
                 <div className="grid gap-1.5 font-normal">
                   <p className="flex text-sm leading-none font-medium">
--- src/data/spots.ts
+++ src/data/spots.ts
@@ -10,15 +10,15 @@ export const spots = [
       lng: 174.7655068135736
     },
     {
-        id: 2,
-        image: "/youngdandy.webp",
-        title: "Young Dandy Cafe",
-        description: "Breakfast, Lunch & Brunch vibes. Best for the start of the day and good to feel social",
-        address: "118 Parnell Road, Parnell",
-        tags: ["Bright", "Gluten Free", "Social", "Free WiFi"],
-        lat: -36.852866259828716, 
-        lng: 174.77918332266307,
-        wifiPassword: "SPARK-VJ4GLO:CuriousVultureYY2%"
+      id: 2,
+      image: "/youngdandy.webp",
+      title: "Young Dandy Cafe",
+      description: "Breakfast, Lunch & Brunch vibes. Best for the start of the day and good to feel social",
+      address: "118 Parnell Road, Parnell",
+      tags: ["Bright", "Gluten Free", "Social", "Free WiFi"],
+      lat: -36.852866259828716, 
+      lng: 174.77918332266307,
+      wifiPassword: "SPARK-VJ4GLO:CuriousVultureYY2%"
     },
     {
       id: 3,
@@ -39,5 +39,45 @@ export const spots = [
       tags: ["Bright", "Gluten Free", "Social", "Free WiFi"],
       lat: -36.8478347475011, 
       lng: 174.7663314803536
+    },
+    {
+      id: 5,
+      image: "/dennys.jpg",
+      title: "Denny's Hobson Street",
+      description: "This is a 24/7 destination with good filter coffee and a dinner vibe. Good for group work",
+      address: "43 High Street, Auckland Central",
+      tags: ["Bright", "Free WiFi", "Easy parking"],
+      lat: -36.84747704712513, 
+      lng: 174.7616658221563
+    },
+    {
+      id: 6,
+      image: "/revelry.jpg",
+      title: "Revelry",
+      description: "A late night study option for those that want to get a wine in with with some work",
+      address: "106 Ponsonby Road, Ponsonby",
+      tags: ["Dim", "Social", "Free WiFi"],
+      lat: -36.85729391785409, 
+      lng: 174.74830110642304 
+    },
+    {
+      id: 7,
+      image: "/uoa-library.jpg",
+      title: "University of Auckland General Library",
+      description: "This place is made for study, and as a non-student you can stroll in and pick a desk to study at",
+      address: "Building 109/5 Alfred Street, Auckland Central",
+      tags: ["Bright", "Free WiFi", "Easy parking", "Library"],
+      lat: -36.85117912131244, 
+      lng: 174.76930989517382
+    },
+    {
+      id: 8,
+      image: "/auckland-domain.jpg",
+      title: "Auckland Domain",
+      description: "A great place to relax under a tree and get into your nature core vibes",
+      address: "Park Road, Parnell",
+      tags: ["Bright","Easy parking"],
+      lat: -36.85822107641934, 
+      lng: 174.77543905143278
     },
   ];
